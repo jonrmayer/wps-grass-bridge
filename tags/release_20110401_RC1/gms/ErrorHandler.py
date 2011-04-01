ec1fac03f380867a6132cead23fc643ee18d4462
soerengebbert <soerengebbert@23da3d23-e2f9-862c-be8f-f61c6c06f202>
2011-04-01 17:37:48 +0000

    First release candidate of wps-grass-bridge with PyWPS, ZOO and 52N WPS support

--- tags/release_20110401_RC1/gms/ErrorHandler.py
+++ tags/release_20110401_RC1/gms/ErrorHandler.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ################################################################################
 # Author:	Soeren Gebbert
 #
@@ -21,18 +20,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-# This file contains global variables which are used by PyWPSGrassModuleStarter 
-# and ZOOGrassModuleStarter to work properly
-
-# !!!!! EDIT THIS SECTION !!!!!
-# Some default variables. Override them to your needs.
-WORKDIR="/tmp/pywps/"
-OUTPUTDIR="/tmp/pywps/"
-LOGFILE="/tmp/pywps/logfile.txt"
-LOGFILE_MODULE_STDOUT="/tmp/pywps/logfile_module_stdout.txt"
-LOGFILE_MODULE_STDERR="/tmp/pywps/logfile_module_sterr.txt"
-GRASS_GIS_BASE="/home/soeren/src/grass7.0/grass_trunk/dist.x86_64-unknown-linux-gnu"
-GRASS_ADDON_PATH="/home/soeren/src/wps-grass-bridge/trunk/gms/Testing/Python/GrassAddons"
-GRASS_VERSION="7.0.svn"
-# !!!!! END EDIT SECTION !!!!!
+class GMSError(Exception):
+    """A very simple error handler for GrassModuleStarter"""
+    pass
 
